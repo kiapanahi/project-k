c35d0213754c85abb9e2afc244fdded59944d8d1
Kia Raad <kia.panahirad@gmail.com>
2023-11-10 15:47:26 +0330

    Add docker compose file for a 3-node kafka cluster with poweshell and bash management scriopts

---
+++ compose/Manage-Cluster.ps1
@@ -0,0 +1,51 @@
+param (
+    [string]$ProjectName = "kafka-cluster",
+    [string]$Action
+)
+
+# Function to start the Kafka cluster
+function Start-Cluster {
+    Write-Host ("Starting Zookeeper for {0}..." -f $ProjectName) -ForegroundColor Green
+    docker-compose -p $ProjectName up -d zookeeper
+
+    Write-Host ("Starting Kafka brokers for {0}..." -f $ProjectName) -ForegroundColor Green
+    docker-compose -p $ProjectName up -d kafka1 kafka2 kafka3
+
+    Write-Host "Kafka cluster started successfully." -ForegroundColor Green
+}
+
+# Function to stop the Kafka cluster
+function Stop-Cluster {
+    Write-Host ("Stopping Kafka cluster for {0}..." -f $ProjectName) -ForegroundColor Red
+    docker-compose -p $ProjectName down
+    Write-Host "Kafka cluster stopped." -ForegroundColor Red
+}
+
+# Function to restart the Kafka cluster
+function Restart-Cluster {
+    Write-Host ("Restarting Kafka cluster for {0}..." -f $ProjectName) -ForegroundColor Red
+
+    # Stopping the cluster
+    Stop-Cluster
+
+    # Removing persistent data volumes
+    Write-Host ("Removing persistent data volumes for {0}..." -f $ProjectName) -ForegroundColor Red
+    docker volume rm "${ProjectName}_kafka1", "${ProjectName}_kafka2", "${ProjectName}_kafka3"
+    docker volume rm "${ProjectName}_zookeeper"
+
+    # Starting the cluster
+    Start-Cluster
+
+    Write-Host "Kafka cluster restarted successfully." -ForegroundColor Red
+}
+
+# Parse the command-line arguments
+switch ($Action) {
+    "start" { Start-Cluster }
+    "stop" { Stop-Cluster }
+    "restart" { Restart-Cluster }
+    default {
+        Write-Host ("Usage: .\manage-cluster.ps1 -ProjectName <project-name> -Action {start|stop|restart}") -ForegroundColor Red
+        exit 1
+    }
+}
---
+++ compose/manage-cluster.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Script to manage a Kafka cluster
+
+# Check if the project name argument is provided, default to "kafka-cluster"
+PROJECT_NAME=${1:-"kafka-cluster"}
+
+# Define colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function to start the Kafka cluster
+start_cluster() {
+    echo -e "${GREEN}Starting Zookeeper for $PROJECT_NAME...${NC}"
+    docker-compose -p $PROJECT_NAME up -d zookeeper
+
+    echo -e "${GREEN}Starting Kafka brokers for $PROJECT_NAME...${NC}"
+    docker-compose -p $PROJECT_NAME up -d kafka1 kafka2 kafka3
+
+    echo -e "${GREEN}Kafka cluster started successfully.${NC}"
+}
+
+# Function to stop the Kafka cluster
+stop_cluster() {
+    echo -e "${RED}Stopping Kafka cluster for $PROJECT_NAME...${NC}"
+    docker-compose -p $PROJECT_NAME down
+    echo -e "${RED}Kafka cluster stopped.${NC}"
+}
+
+# Function to restart the Kafka cluster
+restart_cluster() {
+    echo -e "${RED}Restarting Kafka cluster for $PROJECT_NAME...${NC}"
+
+    # Stopping the cluster
+    stop_cluster
+
+    # Removing persistent data volumes
+    echo -e "${RED}Removing persistent data volumes for $PROJECT_NAME...${NC}"
+    docker volume rm ${PROJECT_NAME}_kafka1 ${PROJECT_NAME}_kafka2 ${PROJECT_NAME}_kafka3
+    docker volume rm ${PROJECT_NAME}_zookeeper
+
+    # Starting the cluster
+    start_cluster
+
+    echo -e "${RED}Kafka cluster restarted successfully.${NC}"
+}
+
+# Parse the command-line arguments
+case "$2" in
+    "start")
+        start_cluster
+        ;;
+    "stop")
+        stop_cluster
+        ;;
+    "restart")
+        restart_cluster
+        ;;
+    *)
+        echo -e "${RED}Usage: $0 <project-name> {start|stop|restart}${NC}"
+        exit 1
+        ;;
+esac
